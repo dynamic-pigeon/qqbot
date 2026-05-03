85ff0643e8140188524f8a9b723a7be7908bc94e
Dynamic_Pigeon <3040313336@qq.com>
2026-05-03 11:16:47 +0800

    update log

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use kovi::build_bot;
+use tracing_appender::rolling::Rotation;
 use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
 fn main() {
@@ -15,8 +16,27 @@ fn main() {
         )
     });
 
+    let file_appender = tracing_appender::rolling::Builder::new()
+        .filename_prefix("bot")
+        .filename_suffix("log")
+        .max_log_files(30)
+        .rotation(Rotation::DAILY)
+        .build("./logs")
+        .unwrap();
+
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
     tracing_subscriber::registry()
         .with(env_filter)
+        .with(
+            fmt::layer()
+                .with_target(true)
+                .with_level(true)
+                .with_line_number(true)
+                .with_thread_ids(true)
+                .with_ansi(false)
+                .with_writer(non_blocking),
+        )
         .with(
             fmt::layer()
                 .with_target(true)
